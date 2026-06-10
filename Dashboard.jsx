39c6a26b5c11295735de0599bbedb31a87c6df69
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 18:09:34 +0300

    Create Dashboard.jsx

---
+++ Dashboard.jsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-4">
+        Dashboard
+      </h1>
+
+      <p className="text-slate-400">
+        Welcome to Taskora.
+      </p>
+    </div>
+  );
+}
