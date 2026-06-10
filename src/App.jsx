f4b2f3b193ae3814cc0e66933f370c602be75a8d
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 18:07:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-950">
-      <div className="bg-slate-900 border border-slate-800 p-10 rounded-3xl shadow-2xl text-center">
-        <h1 className="text-5xl font-bold mb-4">
-          Taskora
-        </h1>
+import AppRoutes from "./routes/AppRoutes";
 
-        <p className="text-slate-400">
-          Professional Productivity Platform
-        </p>
-      </div>
-    </div>
-  );
+function App() {
+  return <AppRoutes />;
 }
 
 export default App;
