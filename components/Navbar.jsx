8c70bc91c397a1987d7ca4806193155659aecfe5
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 18:08:42 +0300

    Create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header className="h-20 border-b border-slate-800 flex items-center justify-between px-8">
+      <h1 className="font-bold text-xl">
+        Taskora
+      </h1>
+
+      <div className="text-slate-400">
+        Productivity Dashboard
+      </div>
+    </header>
+  );
+}
